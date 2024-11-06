9c7479c1f7cb63d1a52db496f1842f5879849505
kkamburov@bitbucket.org <kamburov.kaloyan@gmail.com>
2024-11-06 20:57:57 +0200

    wip

--- src/pages/People/People.page.tsx
+++ src/pages/People/People.page.tsx
@@ -106,7 +106,7 @@ const PeoplePage = () => {
         <div className="person" key={person.id}>
           <span>{person.name}</span>
           <div className="btns-wrapper">
-            <BtnCopy text={window.location.origin + `/${person.name}`} />
+            <BtnCopy text={window.location.origin + `/${person.name.to}`.toLowerCase()} />
 
             <button
               onClick={() => {
